5a9a1ac3eab712996c5f1c1f33dccb9eabd5fc83
Uli <post2uli@gmx.de>
2019-02-20 17:09:14 +0100

    jackson mapper

--- app/src/main/java/de/auli/weatherapp/Student.java
+++ app/src/main/java/de/auli/weatherapp/Student.java
@@ -1,16 +1,7 @@
 package de.auli.weatherapp;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-
-@XmlRootElement
-@XmlAccessorType(XmlAccessType.FIELD)
 public class Student {
-    @XmlElement
     private String firstName;
-    @XmlElement
     private String lastName;
 
     public Student() {
--- app/src/test/java/de/auli/weatherapp/WeatherMarshallTest.java
+++ app/src/test/java/de/auli/weatherapp/WeatherMarshallTest.java
@@ -1,15 +1,11 @@
 package de.auli.weatherapp;
 
-import org.eclipse.persistence.jaxb.MarshallerProperties;
-import org.eclipse.persistence.jaxb.UnmarshallerProperties;
+import com.fasterxml.jackson.databind.AnnotationIntrospector;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.module.jaxb.JaxbAnnotationIntrospector;
+
 import org.junit.Test;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Marshaller;
-import org.eclipse.persistence.oxm.MediaType;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -22,11 +18,14 @@ public class WeatherMarshallTest {
         //String result = "{\"coord\":{\"lon\":8.81,\"lat\":53.08},\"weather\":[{\"id\":801,\"main\":\"Clouds\",\"description\":\"Ein paar Wolken\",\"icon\":\"02d\"}],\"base\":\"stations\",\"main\":{\"temp\":282.15,\"pressure\":1021,\"humidity\":76,\"temp_min\":282.15,\"temp_max\":282.15},\"visibility\":10000,\"wind\":{\"speed\":5.7,\"deg\":220},\"clouds\":{\"all\":20},\"dt\":1550663400,\"sys\":{\"type\":1,\"id\":1281,\"message\":0.004,\"country\":\"DE\",\"sunrise\":1550644297,\"sunset\":1550681169},\"id\":2944388,\"name\":\"Bremen\",\"cod\":200}";
         //System.out.println(result);
 
-        Student s = new Student("Walter", "White");
-        JAXBContext jc = JAXBContext.newInstance(Student.class);
-        Marshaller m = jc.createMarshaller();
+        Student user = new Student("Walter", "White");
 
-        m.setProperty("media-type", "application/json");
-        //u.setProperty(UnmarshallerProperties.MEDIA_TYPE, MediaType.APPLICATION_JSON);
+        ObjectMapper mapper = new ObjectMapper();
+        AnnotationIntrospector introspector = new JaxbAnnotationIntrospector(mapper.getTypeFactory());
+        mapper.setAnnotationIntrospector(introspector);
+
+        String result = mapper.writeValueAsString(user);
+
+        System.out.println(result);
     }
 }
